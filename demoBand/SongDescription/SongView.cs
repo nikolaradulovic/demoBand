d5013cf9f307559aeaff9f5bc14e977c8280a2c9
Nikola Radulovic <nikola.radulovic_bgd@hotmail.com>
2015-04-05 16:40:37 +0200

    sonViewStrophe

---
+++ demoBand/SongDescription/SongView.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace demoBand.SongDescription
+{
+    public class SongView
+    {
+        private List<Strophe> strophes;
+
+        public List<Strophe> Strophes
+        {
+            get { return strophes; }
+            set { strophes = value; }
+        }
+
+        public void addStrophe(Strophe strophe) 
+        {
+            strophes.Add(strophe);
+        }
+
+        public SongView() 
+        {
+            strophes = new List<Strophe>();
+        }
+
+        public SongView(string path)
+        {
+            //iz parsera ucitati listu
+            populateList(path);
+            Task.Delay(250);
+        }
+
+        public  async Task populateList(string path)
+        {
+          
+            try
+            {
+                StorageFile sFile = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(@"Assets\Json\otherside.txt");
+                 var fileStream = await sFile.OpenStreamForReadAsync();
+            using (StreamReader sr = new StreamReader(fileStream))
+            {
+                string line = await sr.ReadToEndAsync();
+           
+                strophes = (List<Strophe>)Newtonsoft.Json.JsonConvert.DeserializeObject(line, typeof(List<Strophe>));
+            }
+            }
+            catch (Exception ex)
+            {
+
+                string ex1 = ex.Message;
+            }
+
+           
+
+
+
+        }
+
+        public async static Task<SongView> createSongView(string path) 
+        {
+            SongView sw = new SongView();
+            await sw.populateList(path);
+            return sw;
+        }
+
+    }
+}
---
+++ demoBand/SongDescription/Strophe.cs
@@ -0,0 +1,49 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoBand.SongDescription
+{
+   // [ParseClassName("Strophe")]
+    public class Strophe 
+    {
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+        private int start;
+
+        public int Start
+        {
+            get { return start; }
+            set { start = value; }
+        }
+        private int end;
+
+        public int End
+        {
+            get { return end; }
+            set { end = value; }
+        }
+
+
+        public Strophe(string text, int start, int end)
+        {
+            this.text = text;
+            this.start = start;
+            this.end = end;
+        }
+
+        public Strophe()
+        {
+
+        }
+
+    }
+}
