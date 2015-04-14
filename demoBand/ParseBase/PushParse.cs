5a6f54f4bafb01d7bedc1607fd84a24a38cecdad
Nikola Radulovic <nikola.radulovic_bgd@hotmail.com>
2015-04-14 20:14:46 +0200

    pushparseFormat

--- demoBand/ParseBase/PushParse.cs
+++ demoBand/ParseBase/PushParse.cs
@@ -16,7 +16,7 @@ namespace demoBand.ParseBase
             push.Query = from user in ParseInstallation.Query
                          where user.Get<string>("username") == collaborator
                          select user;
-            push.Alert = "You have new request for collaboration\nTitle: "+songname+"\n"+"author: "+artistname;
+            push.Alert = "Collaboration request\nTitle:/t"+songname+"\n"+"Author:\t"+artistname;
 
             await push.SendAsync();
         }
