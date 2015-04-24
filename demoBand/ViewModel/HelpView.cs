a350c364984bd76e99208a8a95ebb5f2be91633f
Nikola Radulovic <nikola.radulovic_bgd@hotmail.com>
2015-04-24 12:20:14 +0200

    helpScreens

--- demoBand/ViewModel/HelpView.cs
+++ demoBand/ViewModel/HelpView.cs
@@ -58,16 +58,16 @@ namespace demoBand.ViewModel
        public HelpView()
        {
            helpCreateList = new List<HelpElement>();
-           helpCreateList.Add(new HelpElement("Fist click on \"Create\" tab", "../Assets/Images/Backgrounds/login-background.png"));
-           helpCreateList.Add(new HelpElement("Then choose your instrument", "../Assets/Images/Backgrounds/login-background.png"));
-           helpCreateList.Add(new HelpElement("...and start recording", "../Assets/Images/Backgrounds/login-background.png"));
-           helpCreateList.Add(new HelpElement("Save song on the end", "../Assets/Images/Backgrounds/login-background.png"));
+           helpCreateList.Add(new HelpElement("Fist click on \"Create\" tab", "../Assets/Images/Help/create.png"));
+           helpCreateList.Add(new HelpElement("Then choose your instrument", "../Assets/Images/Help/choosePiano.png"));
+           helpCreateList.Add(new HelpElement("...and start recording", "../Assets/Images/Help/startRec.png"));
+           helpCreateList.Add(new HelpElement("Save song at the end", "../Assets/Images/Help/saveSong.png"));
 
            helpCollaborateList = new List<HelpElement>();
-           helpCollaborateList.Add(new HelpElement("Fist click on \"My Songs\" tab", "../Assets/Images/Backgrounds/login-background.png"));
-           helpCollaborateList.Add(new HelpElement("Find song you want to collaborate on", "../Assets/Images/Backgrounds/login-background.png"));
-           helpCollaborateList.Add(new HelpElement("Choose desired instruments", "../Assets/Images/Backgrounds/login-background.png"));
-           helpCollaborateList.Add(new HelpElement("...and start collaborating", "../Assets/Images/Backgrounds/login-background.png"));
+           helpCollaborateList.Add(new HelpElement("First click on \"My Songs\" tab", "../Assets/Images/Help/mySongs.png"));
+           helpCollaborateList.Add(new HelpElement("Find song you want to collaborate on", "../Assets/Images/Help/pickSong.png"));
+           helpCollaborateList.Add(new HelpElement("Choose desired instruments", "../Assets/Images/Help/pickInstruments.png"));
+           helpCollaborateList.Add(new HelpElement("...and start collaborating", "../Assets/Images/Help/startCollab.png"));
        }
     }
 }
